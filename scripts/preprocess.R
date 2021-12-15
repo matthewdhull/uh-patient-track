822876145fa364c0b5ead9702fbdcda7a5a80f2b
Matthew Hull <matthewdhull@gmail.com>
2021-12-14 20:12:16 -0800

    ingest uh aot data, check req'd sheets

--- scripts/preprocess.R
+++ scripts/preprocess.R
@@ -1,1 +1,9 @@
 print('...preprocessing...')
+
+file <- "data/UH AOT Data Report_ Oct 2021.xlsx"
+sheets <- excel_sheets(file)
+
+required_sheets_csv <- read.csv2("data/required_sheets.csv")['x']
+required_sheets <- as.vector(as_vector(required_sheets_csv))
+missing_sheets <- setdiff(required_sheets,sheets)
+print(paste("found",length(missing_sheets), "missing sheets"))
