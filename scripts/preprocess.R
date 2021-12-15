701cd4d7f09aca1898aa26fe79b71a4dc065eb81
Matthew Hull <matthewdhull@gmail.com>
2021-12-15 12:07:07 -0800

    ingest uh data into dfs

--- scripts/preprocess.R
+++ scripts/preprocess.R
@@ -7,3 +7,22 @@ required_sheets_csv <- read.csv2("data/required_sheets.csv")['x']
 required_sheets <- as.vector(as_vector(required_sheets_csv))
 missing_sheets <- setdiff(required_sheets,sheets)
 print(paste("found",length(missing_sheets), "missing sheets"))
+
+
+# read in Sheets 2-12 and create dataframes
+# these sheets have col names at row 5
+# sheets 1, 13, 14 have col names at row 1
+
+data = {}
+sheet_idx <- c(2:12) # modify this to import another range of sheets, e.g., c(1, 2, 5)
+for (i in sheet_idx){
+  sheet <- read_excel(file, sheet = i)
+  sheet_col_names <- unname(as_vector(sheet[5,]))  # colnames always on this row?
+  sheet_data <- sheet[6:nrow(sheet),] # data always begins on this row?
+  colnames(sheet_data) <- sheet_col_names
+  data[[sheets[i]]] <- sheet_data
+  write_excel_csv2(sheet_data, paste("data/",sheets[i],".csv",sep=""),delim=",")
+}
+
+print('preprocessing complete.')
+
