16de626eb6f3e3608ad513401297d357b03ce7c0
Matthew Hull <matthewdhull@gmail.com>
2022-03-15 15:58:06 -0700

    import lookup noms code lookup table

--- scripts/preprocess_noms.R
+++ scripts/preprocess_noms.R
@@ -1,14 +1,18 @@
 print('...preprocessing NOMS data...')
 
-file <- "data/NOMS.csv"
-noms_df <- read.csv2(file, sep=",")
+noms_file <- "data/NOMS.csv"
+code_lookup_file <- "data/CMHSNOMsAdultClientLevelMeasuresCodebook_20210204.xlsx"
+noms_df <- read.csv2(noms_file, sep=",")
+code_lookup_sheet <- read_excel(code_lookup_file)
 
 # TODO - 
-# rm trailing ' from 
+# rm trailing ' from `ConsumerID`
+# verify required cols
+# import lookup table
+# implement code lookup for req'd cols
 
+# preprocess_args <- list(file, idx, 5, 6, TRUE)
+# noms_data <- do.call(fun, preprocess_args) # write out preprocessed data
 
-preprocess_args <- list(file, idx, 5, 6, TRUE)
-noms_data <- do.call(fun, preprocess_args)
-
-print('preprocessing complete.')
+print('preprocessing NOMS data complete.')
 
